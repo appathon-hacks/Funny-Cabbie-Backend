34e1a3dd85655154781b4d6bd60b17da0cc5489b
maharsh007 <maharshi.choudhury@gmail.com>
2015-02-08 02:57:42 +0530

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+
+
+from flask import Flask, jsonify, make_response, render_template, redirect, request
+from crossdomain import crossdomain
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@crossdomain(origin='*')
+def index():
+    '''
+    This page is displayed when index page is requested.
+    '''
+    return render_template('main.html')
+	
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
