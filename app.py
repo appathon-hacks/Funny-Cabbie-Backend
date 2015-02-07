b62614b24905614611f5fe0b74dc464734231559
maharsh007 <maharshi.choudhury@gmail.com>
2015-02-08 03:42:14 +0530

    updated

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from crossdomain import crossdomain
 
 app = Flask(__name__)
 
+commands = [
+	'joke'
+];
+
 
 @app.route('/')
 @crossdomain(origin='*')
@@ -12,7 +16,42 @@ def index():
     '''
     This page is displayed when index page is requested.
     '''
-    return render_template('main.html')
+    return  make_response(jsonify({ 'error': '503 something wrong' }))
+	
+@app.route('/command/<command_name>/')
+@crossdomain(origin='*')
+def command(command_name):
+	'''
+	This page is displayed when index page is requested.
+	'''
+	print command_name
+	print request.args['args']
+	return  make_response(jsonify({ 'error': '500' }))	
+	
+
+	
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    Returns a jsonified 404 error message instead of a HTTP 404 error.
+    '''
+    return make_response(jsonify({ 'error': '404 not found' }), 404)
+
+
+@app.errorhandler(503)
+def not_found(error):
+    '''
+    Returns a jsonified 503 error message instead of a HTTP 404 error.
+    '''
+    return make_response(jsonify({ 'error': '503 something wrong' }), 503)
+	
+@app.errorhandler(500)
+def not_found(error):
+	'''
+	Returns a jsonified 500 error message instead of a HTTP 404 error.
+	'''
+	print error
+	return make_response(jsonify({ 'error': '500 something wrong' }), 500)	
 	
 
 
